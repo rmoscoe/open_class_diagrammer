0ab94e09b52ebc8549e3c7c21a7ba4b28c6ddb03
Ryan Moscoe <ryanmoscoe@gmail.com>
2024-10-10 21:10:15 -0700

    initial commit

---
+++ open_class_diagrammer/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for open_class_diagrammer project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'open_class_diagrammer.settings')
+
+application = get_wsgi_application()
